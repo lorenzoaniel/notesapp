06eb602c09be6a86af62d89d8e0ea24e7e71a92c
lorenzoaniel <lorenzoaniel@gmail.com>
2023-03-11 21:29:19 -0700

    index: refactored logic to app

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,15 @@
-import express from "express";
+import app from "./app";
 import mongoose from "mongoose";
-import "dotenv/config"; //needs to be in this format or will have missing env variables
 import env from "./utils/validateEnv";
 
-const app = express();
-
-app.get("/", (req, res) => {
-	res.send("Hello, World!");
-});
+const port = env.PORT;
 
 mongoose
 	.connect(env.MONGO_URL)
 	.then(() => {
 		console.log("Mongoose Connected");
-		app.listen(env.PORT, () => {
-			console.log("server started" + env.PORT);
+		app.listen(port, () => {
+			console.log("server started " + port);
 		});
 	})
 	.catch(console.error);
