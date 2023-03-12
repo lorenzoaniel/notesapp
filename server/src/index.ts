1265d305d91964cc921d39c4a171834baa438e9a
lorenzoaniel <lorenzoaniel@gmail.com>
2023-03-11 20:22:43 -0700

    mongoose connection and env validation
    connection will now crash if env does not work and also gets rid of TS null complaint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import "dotenv/config"; //needs to be in this format or will have missing env variables
+import env from "./utils/validateEnv";
 
 const app = express();
-const port = undefined;
 
 app.get("/", (req, res) => {
 	res.send("Hello, World!");
 });
 
-app.listen(port, () => {
-	console.log("server started" + port);
-});
+mongoose
+	.connect(env.MONGO_URL)
+	.then(() => {
+		console.log("Mongoose Connected");
+		app.listen(env.PORT, () => {
+			console.log("server started" + env.PORT);
+		});
+	})
+	.catch(console.error);
