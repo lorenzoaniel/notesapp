020c224ea94ed146333bd1749dbbf328703fa360
lorenzoaniel <lorenzoaniel@gmail.com>
2023-03-12 18:59:43 -0600

    notes: added create notes and get single note controller

--- server/src/controllers/notes.ts
+++ server/src/controllers/notes.ts
@@ -13,3 +13,33 @@ export const getNotes: RequestHandler = async (req, res, next) => {
 		next(error);
 	}
 };
+
+export const createNotes: RequestHandler = async (req, res, next) => {
+	const title = req.body.title;
+	const text = req.body.text;
+
+	try {
+		//send new note to client and DB, similar to .find() but .create() auto returns a promise by default so no need for .exec()
+		const newNote = await NoteModel.create({
+			title: title,
+			text: text,
+		});
+
+		//response sent back to client
+		res.status(201).json(newNote);
+	} catch (error) {
+		next(error);
+	}
+};
+
+export const getNote: RequestHandler = async (req, res, next) => {
+	const noteId = req.params.noteId; //same spelling as notes route since params will take values in url ex: notes/:noteId <- this will be replaced by a note id in client side and then retrieved by backend (probably a list of all notes saved in client side)
+
+	try {
+		const note = await NoteModel.findById(noteId).exec();
+
+		res.status(200).json(note);
+	} catch (error) {
+		next(error);
+	}
+};
