bedb3299c74ffa4bae523db796e671e7ac493562
lorenzoaniel <lorenzoaniel@gmail.com>
2023-03-12 19:54:05 -0600

    missing title, invalid id note error handling

--- server/src/controllers/notes.ts
+++ server/src/controllers/notes.ts
@@ -1,3 +1,5 @@
+import mongoose from "mongoose";
+import createHttpError from "http-errors";
 import { RequestHandler } from "express";
 import NoteModel from "../models/note";
 
@@ -6,6 +8,7 @@ export const getNotes: RequestHandler = async (req, res, next) => {
 	try {
 		//this route handler then uses NoteModel to find all related obj that fits this schema and send them back as a JSON array
 		const notes = await NoteModel.find().exec();
+
 		//this array is sent back as a res stat 200 which means OK
 		res.status(200).json(notes);
 	} catch (error) {
@@ -14,11 +17,24 @@ export const getNotes: RequestHandler = async (req, res, next) => {
 	}
 };
 
-export const createNotes: RequestHandler = async (req, res, next) => {
+interface CreateNoteBody {
+	//even if it is required there might be an instance where title is missing we want to handle that in the requesthandler and not throw a different error here
+	title?: string;
+	text?: string;
+}
+
+export const createNotes: RequestHandler<unknown, unknown, CreateNoteBody, unknown> = async (
+	req,
+	res,
+	next
+) => {
 	const title = req.body.title;
 	const text = req.body.text;
 
 	try {
+		//400 bad request since title is missing
+		if (!title) throw createHttpError(400, "Note must have a title");
+
 		//send new note to client and DB, similar to .find() but .create() auto returns a promise by default so no need for .exec()
 		const newNote = await NoteModel.create({
 			title: title,
@@ -36,7 +52,11 @@ export const getNote: RequestHandler = async (req, res, next) => {
 	const noteId = req.params.noteId; //same spelling as notes route since params will take values in url ex: notes/:noteId <- this will be replaced by a note id in client side and then retrieved by backend (probably a list of all notes saved in client side)
 
 	try {
+		if (!mongoose.isValidObjectId(noteId)) throw createHttpError(400, "Invalid note id");
+
 		const note = await NoteModel.findById(noteId).exec();
+
+		if (!note) throw createHttpError(404, "note not found");
 
 		res.status(200).json(note);
 	} catch (error) {
