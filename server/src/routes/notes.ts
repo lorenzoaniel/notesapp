591e30bcb256d66a12c962dd121a3b08c8ad95cb
lorenzoaniel <lorenzoaniel@gmail.com>
2023-03-12 19:11:20 -0600

    note routes: create and get note

--- server/src/routes/notes.ts
+++ server/src/routes/notes.ts
@@ -1,9 +1,15 @@
 import express from "express";
 import * as NotesController from "../controllers/notes";
 
-const router = express();
+const router = express.Router();
 
 //express route handler responds to HTTP GET req to root URL '/'
 router.get("/", NotesController.getNotes);
+
+//same endpoint since different HTTP type, this one sends a note to the collection
+router.post("/", NotesController.createNotes);
+
+//base url plus noteId param that is then used in controller passed to it
+router.get("/:noteId", NotesController.getNote);
 
 export default router;
