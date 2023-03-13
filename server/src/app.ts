d6ca1eb4d2e7d3750d608b80abc716208fd24cdf
lorenzoaniel <lorenzoaniel@gmail.com>
2023-03-12 19:53:36 -0600

    added endpoint unknown erro

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,7 @@ import "dotenv/config"; //used to load env vars from env file
 import express, { Request, Response, NextFunction } from "express";
 import morgan from "morgan";
 import notesRoutes from "./routes/notes";
+import createHttpError, { isHttpError } from "http-errors";
 
 //create express instance
 const app = express();
@@ -17,7 +18,8 @@ app.use("/api/notes", notesRoutes);
 
 //end point not found error, this one is infered automatically unlike one below, works for normal routes but not error routes
 app.use((req, res, next) => {
-	next(Error("endpoint not found"));
+	//resource not found
+	next(createHttpError(404, "This endpoint does not exist"));
 });
 
 //error-handling middleware
@@ -26,10 +28,13 @@ app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
 	console.error(error);
 	//default error message
 	let errorMessage = "An Unknown Error occurred";
-	//server error status
-	const statusCode = 500;
+	//internal server error status fallback
+	let statusCode = 500;
 	//if error is a known error, or instance of generic Error class replace generic message with a known error message defined in Error class
-	if (error instanceof Error) errorMessage = error.message;
+	if (isHttpError(error)) {
+		statusCode = error.status;
+		errorMessage = error.message;
+	}
 	//else it sends the generic errorMessage
 	res.status(statusCode).json({ error: errorMessage });
 });
