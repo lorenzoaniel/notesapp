9a53d89dec68d3e442ee5a3698100fcb00a0b8c3
lorenzoaniel <lorenzoaniel@gmail.com>
2023-03-15 21:21:18 -0600

    Update Home.tsx

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,14 +1,75 @@
 import { motion } from "framer-motion";
 import React from "react";
 import styled from "styled-components";
+import Note from "../../components/Note";
+import { device } from "../../styles/breakpoints";
+import AddButton from "../../components/buttons/AddButton";
 
 const Home: React.FC = () => {
-	return <Main>home</Main>;
+	return (
+		<Main>
+			<AddButton title="Add Note" />
+			<Board>
+				<Note
+					text="testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest"
+					title={"Test1"}
+				/>
+				<Note />
+				<Note />
+				<Note />
+				<Note />
+				<Note />
+				<Note />
+				<Note />
+				<Note />
+				<Note />
+				<Note />
+				<Note />
+			</Board>
+		</Main>
+	);
 };
 
-const Main = styled(motion.nav)`
-	border: 0.1rem solid purple;
-	height: 10vh;
-`;
+const Main = styled(motion.section)(
+	({ theme }) => `
+	${theme.mixins.flex.flxCntrCntr}
+	height: 100%;
+	padding: 1rem;
+	overflow: hidden;
+	display: flex;
+	flex-direction: column;
+`
+);
+
+const Board = styled(motion.div)(
+	({ theme }) => `
+	height: 90%;
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+	row-gap: 1rem;
+	padding: 1rem;
+	overflow-y: scroll;
+
+	/* Hide scrollbar for Chrome and Edge */
+	&::-webkit-scrollbar {
+		width: 0.5rem;
+		height: 0.5rem;
+	}
+
+	&::-webkit-scrollbar-thumb {
+		background-color: rgb(${theme.color.secondary.medium});
+		border-radius: 0.4rem;
+	}
+
+	@media ${device.tablet} {
+		flex-direction: row;
+		flex-wrap: wrap;
+		column-gap: 1rem;
+		align-items: flex-start;
+		justify-content: center;
+	}
+`
+);
 
 export default Home;
