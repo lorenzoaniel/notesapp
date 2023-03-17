c062b5e5a0a6dbfab3bade93fe0d83015771e52b
lorenzoaniel <lorenzoaniel@gmail.com>
2023-03-17 14:24:02 -0600

    Layout: added footer component

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -14,6 +14,9 @@ const Layout: React.FC<Props> = ({ children }) => {
 				<Navbar />
 			</Header>
 			<Content>{children}</Content>
+			<Footer>
+				<small style={{ textAlign: "center" }}>&copy; Copyright 2023, Mikhail Lorenzo Aniel</small>
+			</Footer>
 		</Main>
 	);
 };
@@ -36,8 +39,22 @@ const Header = styled.header`
 const Content = styled.section(
 	({ theme }) => `
   ${theme.mixins.flex.flxCntrCntr}
-  height: 90%;
+  height: 85%;
 	overflow: hidden;
+`
+);
+
+const Footer = styled.section(
+	({ theme }) => `
+  ${theme.mixins.flex.flxCntrCntr}
+  height: 5%;
+	overflow: hidden;
+	background: rgb(${theme.color.primary.dark});
+	box-shadow: 0 0 1rem 0.5rem rgb(${theme.color.primary.dark}), 0 0 1rem 0.1rem rgb(0,0,0,0.5) inset;
+	border-radius: 1rem 1rem 0 0;
+
+	color: rgb(${theme.color.secondary.dark});
+	text-shadow: ${theme.font.shadow.dark};
 `
 );
 
