517c6cc0a33cba149dc06e9242103d85c8f5135d
lorenzoaniel <lorenzoaniel@gmail.com>
2023-03-17 13:33:25 -0600

    Note: All features functioning including error handling also added suspense base
    will create a suspense component later,

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,40 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { ThemeProvider } from "styled-components";
 import { Outlet, Route, Routes } from "react-router";
 
 import { GlobalStyle } from "./styles/GlobalStyle";
 import base from "./styles/themes/base";
 
-import Layout from "./components/Layout";
-import Home from "./pages/home/Home";
-import Privacy from "./pages/privacy/Privacy";
+// import Layout from "./components/Layout";
+// import Home from "./pages/home/Home";
+// import Privacy from "./pages/privacy/Privacy";
 import { AnimatePresence } from "framer-motion";
+
+const Layout = lazy(() => import("./components/Layout"));
+const Home = lazy(() => import("./pages/home/Home"));
+const Privacy = lazy(() => import("./pages/privacy/Privacy"));
 
 const App: React.FC = () => {
 	return (
 		<ThemeProvider theme={base}>
 			<GlobalStyle />
 			<AnimatePresence>
-				<Routes>
-					<Route
-						path="/"
-						element={
-							<Layout>
-								<Outlet />
-							</Layout>
-						}
-					>
-						<Route index element={<Home />} />
-						<Route path="home" element={<Home />} />
-						<Route path="privacy" element={<Privacy />} />
-					</Route>
-				</Routes>
+				<Suspense fallback={<div>Loading...</div>}>
+					<Routes>
+						<Route
+							path="/"
+							element={
+								<Layout>
+									<Outlet />
+								</Layout>
+							}
+						>
+							<Route index element={<Home />} />
+							<Route path="home" element={<Home />} />
+							<Route path="privacy" element={<Privacy />} />
+						</Route>
+					</Routes>
+				</Suspense>
 			</AnimatePresence>
 		</ThemeProvider>
 	);
