340b2edf321d1bbfe3914317d8fc929c98e7a8b3
lorenzoaniel <lorenzoaniel@gmail.com>
2023-03-16 22:05:55 -0600

    Update Note.tsx

--- client/src/components/Note.tsx
+++ client/src/components/Note.tsx
@@ -1,30 +1,61 @@
 import { motion } from "framer-motion";
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
+import { TypeNote } from "../models/note";
+
 import { GiTrashCan } from "react-icons/gi";
+import { useAppDispatch } from "../redux/hooks";
+import { deleteNote, fetchNotes, updateNote } from "../redux/features/noteApiSlice";
 
-interface Note {
-	title?: string;
-	text?: string;
-	modifiedStatus?: string;
-}
+const Note: React.FC<TypeNote> = ({ _id, text, title, createdAt, updatedAt }) => {
+	const [note, setNote] = useState<TypeNote>({ _id, text, title, createdAt, updatedAt });
+	const dispatch = useAppDispatch();
 
-const Note: React.FC<Note> = ({
-	text = "test Content",
-	title = "test title",
-	modifiedStatus = "test date here",
-}) => {
+	// console.log(note);
+	// console.log({ _id, text, title, createdAt, updatedAt });
+
 	return (
 		<Main>
 			<Header>
-				{title}
-				<IconWrapper>
+				<Title
+					onBlur={async (event) => {
+						await setNote((curr) => {
+							return { ...curr, title: event.target.value };
+						});
+						await dispatch(updateNote({ _id: note._id, title: note.title, text: note.text }));
+						await dispatch(fetchNotes());
+					}}
+					type="text"
+					defaultValue={note.title}
+					maxLength={32}
+				/>
+				<IconWrapper
+					onClick={async () => {
+						await dispatch(deleteNote(note._id));
+						await dispatch(fetchNotes());
+					}}
+					variants={_MotionVariants.IconWrapper}
+					initial="initial"
+					whileHover="whileHover"
+					whileTap="initial"
+				>
 					<GiTrashCan style={icon_style} />
 				</IconWrapper>
 			</Header>
-			<Text>{text}</Text>
-			{modifiedStatus}
+			<Text
+				onBlur={async (event) => {
+					await setNote((curr) => {
+						return { ...curr, text: event.target.value };
+					});
+					await dispatch(updateNote({ _id: note._id, title: note.title, text: note.text }));
+					await dispatch(fetchNotes());
+				}}
+				defaultValue={note.text}
+			/>
+			{note.updatedAt > note.createdAt
+				? "Updated at:" + note.updatedAt
+				: "Created at:" + note.createdAt}
 		</Main>
 	);
 };
@@ -32,14 +63,16 @@ const Note: React.FC<Note> = ({
 const Main = styled(motion.div)(
 	({ theme }) => `
 	${theme.mixins.flex.flxColCntrCntr}
+  background: rgb(${theme.color.primary.medium});
   min-height: 27rem;
 	max-height: 27rem;
 	min-width: 30rem;
 	max-width: 30rem;
-  background: rgb(${theme.color.primary.medium});
+	padding-bottom: 0.5rem;
 
 	border-radius: 0.5rem;
 	box-shadow: 0 0 0.5rem 0.1rem black inset;
+
 	font-family: ${theme.font.style.flower};
 	font-weight: 600;
 `
@@ -56,6 +89,24 @@ const Header = styled(motion.div)(
 `
 );
 
+const Title = styled(motion.input)(
+	({ theme }) => `
+	background: transparent;
+	padding: 0;
+	margin: 0;
+	border: none;
+	height: 3rem;
+	width: 25rem;
+
+	font-family: ${theme.font.style.flower};
+	font-weight: 600;
+	white-space: nowrap;
+	text-overflow: ellipsis;
+	overflow: hidden;
+	word-break: normal;
+`
+);
+
 const IconWrapper = styled(motion.div)(
 	({ theme }) => `
 	height: 3rem;
@@ -69,7 +120,7 @@ const icon_style = {
 	filter: "drop-shadow(0 0rem 0.2rem rgb(0,0,0,1))",
 };
 
-const Text = styled(motion.p)(
+const Text = styled(motion.textarea)(
 	({ theme }) => `
 	background: rgb(${theme.color.primary.light});
 	min-height: 20rem;
@@ -82,9 +133,34 @@ const Text = styled(motion.p)(
 
 	color: black;
 	word-break: break-all;
+	font-family: ${theme.font.style.flower};
+	font-weight: 600;
+	text-shadow: ${theme.font.shadow.light};
 
 	overflow-y: scroll;
 `
 );
+
+const _MotionVariants = {
+	IconWrapper: {
+		initial: {
+			scale: 1,
+			rotate: 0,
+		},
+		whileHover: {
+			scale: 1.1,
+			rotate: 5,
+			transition: {
+				type: "spring",
+				damping: 0,
+				stiffness: 200,
+			},
+		},
+		whileTap: {
+			scale: 0,
+			rotate: 0,
+		},
+	},
+};
 
 export default Note;
