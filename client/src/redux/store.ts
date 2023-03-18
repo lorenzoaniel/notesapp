5d54d466ecc7f631a832c15e0ddc8fca85a51c0b
lorenzoaniel <lorenzoaniel@gmail.com>
2023-03-17 18:15:17 -0600

    Update store.ts

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,9 +1,11 @@
 import { noteApiSlice } from "./features/noteApiSlice";
 import { configureStore } from "@reduxjs/toolkit";
+import { userApiSlice } from "./features/userApiSlice";
 
 export const store = configureStore({
 	reducer: {
 		noteApiSlice: noteApiSlice.reducer,
+		userApiSlice: userApiSlice.reducer,
 	},
 });
 
