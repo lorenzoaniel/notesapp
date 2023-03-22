b9d6dd5aaf83d95a11f3c55b417bb7ea5c4b3680
lorenzoaniel <lorenzoaniel@gmail.com>
2023-03-21 20:09:44 -0600

    Update auth.ts

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -2,11 +2,9 @@ import createHttpError from "http-errors";
 import { RequestHandler } from "express";
 
 export const requiresAuth: RequestHandler = async (req, res, next) => {
-	const authenicatedUserId = req.session.userId;
-
 	if (req.session.userId) {
 		next();
 	} else {
-		next(createHttpError(401, "user not authenticated"));
+		next(createHttpError(401, "User not authenticated"));
 	}
 };
