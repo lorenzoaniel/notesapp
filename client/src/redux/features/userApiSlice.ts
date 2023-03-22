1941217b1f00020d38255d573da4ec84c0eadf00
lorenzoaniel <lorenzoaniel@gmail.com>
2023-03-21 20:08:09 -0600

    userApiSlice: reject case for signup/login extrareducers as well as error state reset
    needed a way to transfer error messages from server to front end, as well as a few front end fixes for persitant error messages when switching forms hence the error reset func

--- client/src/redux/features/userApiSlice.ts
+++ client/src/redux/features/userApiSlice.ts
@@ -1,9 +1,11 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { RootState } from "../store";
 import { User } from "../../models/user";
+import { ConflictError, UnauthorizedError } from "../../errors/http_error";
 
 interface InitialState {
 	user: User;
+	errors: any;
 }
 
 const initialState: InitialState = {
@@ -11,15 +13,23 @@ const initialState: InitialState = {
 		username: null,
 		useremail: null,
 	},
+	errors: undefined,
 };
 
 const fetchData = async (input: RequestInfo, init?: RequestInit) => {
-	const res = await fetch(input, init);
-	if (res.ok) {
-		return res;
+	const response = await fetch(input, { ...init, credentials: "include" });
+	if (response.ok) {
+		return response;
 	} else {
-		const errBdy = await res.json();
-		throw Error(errBdy.error);
+		const errorBody = await response.json();
+		const errorMessage = errorBody.error;
+		if (response.status === 401) {
+			throw new UnauthorizedError(errorMessage);
+		} else if (response.status === 409) {
+			throw new ConflictError(errorMessage);
+		} else {
+			throw Error("Request failed with status: " + response.status + " message: " + errorMessage);
+		}
 	}
 };
 
@@ -27,7 +37,7 @@ const fetchData = async (input: RequestInfo, init?: RequestInit) => {
 export const getLoggedInUser = createAsyncThunk(
 	"userApiSlice/getLoggedInUser",
 	async (): Promise<User> => {
-		const res = await fetchData("/api/users", { method: "GET" });
+		const res = await fetchData("http://localhost:5000/api/users", { method: "GET" });
 		return res.json();
 	}
 );
@@ -42,7 +52,7 @@ interface SignUpCreds {
 export const signUp = createAsyncThunk(
 	"userApiSlice/signUp",
 	async (credentials: SignUpCreds): Promise<User> => {
-		const res = await fetchData("/api/users", {
+		const res = await fetchData("http://localhost:5000/api/users/signup", {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json",
@@ -62,7 +72,7 @@ interface LoginCreds {
 export const login = createAsyncThunk(
 	"userApiSlice/login",
 	async (credentials: LoginCreds): Promise<User> => {
-		const res = await fetchData("/api/users/login", {
+		const res = await fetchData("http://localhost:5000/api/users/login", {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json",
@@ -74,13 +84,17 @@ export const login = createAsyncThunk(
 );
 
 export const logout = createAsyncThunk("userApiSlice/logout", async () => {
-	await fetchData("/api/users/logout", { method: "POST" });
+	await fetchData("http://localhost:5000/api/users/logout", { method: "POST" });
 });
 
 export const userApiSlice = createSlice({
 	name: "userApiSlice",
 	initialState,
-	reducers: {},
+	reducers: {
+		resetError: (state) => {
+			state.errors = undefined;
+		},
+	},
 	extraReducers: (builder) => {
 		builder
 			.addCase(getLoggedInUser.fulfilled, (state, action) => {
@@ -93,10 +107,17 @@ export const userApiSlice = createSlice({
 					state.user = { username: action.payload.username, useremail: action.payload.useremail };
 				}
 			})
+			.addCase(signUp.rejected, (state, action) => {
+				console.log(action.error);
+				state.errors = action.error;
+			})
 			.addCase(login.fulfilled, (state, action) => {
 				if (action.payload) {
 					state.user = { username: action.payload.username, useremail: action.payload.useremail };
 				}
+			})
+			.addCase(login.rejected, (state, action) => {
+				state.errors = action.error;
 			})
 			.addCase(logout.fulfilled, (state) => {
 				state.user = {
@@ -107,6 +128,7 @@ export const userApiSlice = createSlice({
 	},
 });
 
+export const { resetError } = userApiSlice.actions;
 export const selectUserApi = (state: RootState) => state.userApiSlice;
 
 export default userApiSlice.reducer;
