f2ad32256eeec86ee20cdc43569c6901f73d6f7d
lorenzoaniel <lorenzoaniel@gmail.com>
2023-03-21 19:59:02 -0600

    App: sign up and login/signup redirect on login

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,34 @@
-import React, { Suspense, lazy } from "react";
+import React, { Suspense, lazy, useEffect } from "react";
 import { ThemeProvider } from "styled-components";
-import { Outlet, Route, Routes, useLocation } from "react-router";
+import { Navigate, Outlet, Route, Routes, redirect, useLocation } from "react-router";
 
 import { GlobalStyle } from "./styles/GlobalStyle";
 import base from "./styles/themes/base";
 import LoginOrSignUp from "./pages/authentication/LoginOrSignUp";
 import { AnimatePresence } from "framer-motion";
+import { getLoggedInUser, selectUserApi } from "./redux/features/userApiSlice";
+import { useAppDispatch, useAppSelector } from "./redux/hooks";
 
 const Layout = lazy(() => import("./components/Layout"));
 const Home = lazy(() => import("./pages/home/Home"));
 const Privacy = lazy(() => import("./pages/privacy/Privacy"));
 
 const App: React.FC = () => {
+	const user = useAppSelector(selectUserApi).user;
 	const location = useLocation();
+	const dispatch = useAppDispatch();
+
+	useEffect(() => {
+		const auth = async () => {
+			try {
+				await dispatch(getLoggedInUser());
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		auth();
+	}, []);
+
 	return (
 		<ThemeProvider theme={base}>
 			<GlobalStyle />
@@ -30,7 +46,12 @@ const App: React.FC = () => {
 							<Route index element={<Home />} />
 							<Route path="home" element={<Home />} />
 							<Route path="privacy" element={<Privacy />} />
-							<Route path="loginorsignup" element={<LoginOrSignUp />} />
+							<Route
+								path="loginorsignup"
+								element={
+									user.username ? <Navigate to={`/home`} replace={true} /> : <LoginOrSignUp />
+								}
+							/>
 						</Route>
 					</Routes>
 				</Suspense>
