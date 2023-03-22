c40afabc5dface6ab29a86d35dcebab05f0d6940
lorenzoaniel <lorenzoaniel@gmail.com>
2023-03-21 20:10:51 -0600

    validateEnv: added front-end URL for front-end
    need this for CORS cookies, front-end corresponding fix is adding credentials: 'include' in fetch

--- server/src/utils/validateEnv.ts
+++ server/src/utils/validateEnv.ts
@@ -1,11 +1,12 @@
 import { cleanEnv } from "envalid";
-import { port, str } from "envalid/dist/validators"; //has to be imported this way instead of from 'envalid'
+import { port, str, url } from "envalid/dist/validators"; //has to be imported this way instead of from 'envalid'
 
 //1st arg contains env vars to be validated, 2nd an obj that defines what env vars should look like
 export default cleanEnv(process.env, {
 	MONGO_URL: str(), //expected to be a string
 	PORT: port(), //expected to be a port number
 	SESSION_SECRET: str(),
+	ALLOWEDORIGIN: url(),
 });
 
 //an error is thrown if validation fails
