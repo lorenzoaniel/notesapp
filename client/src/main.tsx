707bac100bf899ce652bb8fe1a7cbe425adc3b87
lorenzoaniel <lorenzoaniel@gmail.com>
2023-03-24 16:26:34 -0600

    messing with CORS potential fix

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<HashRouter>
+			<BrowserRouter>
 				<App />
-			</HashRouter>
+			</BrowserRouter>
 		</Provider>
 	</React.StrictMode>
 );
